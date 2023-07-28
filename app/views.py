90f65b068730e6442d5928b3cba833f045a9e35e
Patrahari8 <patrahari8@gmail.com>
2023-07-28 12:03:46 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -8,3 +8,6 @@ def data_render(request):
 def if_else(request):
     d={'a':100,'b':20}
     return render(request,'if_else.html',context=d)
+def if_elif(request):
+    d={'a':100,'b':2000,'c':500000}
+    return render(request,'if_elif.html',context=d)
